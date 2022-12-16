82176794b916d340fa670b1596a86598f5efc22b
Julien Oiknine <julien.oiknine@parapy.nl>
2022-12-16 18:04:13 +0100

    Sliding navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import '../css/Navbar.css';
 
 function Navbar() {
+
+  const location = useLocation();
+  const path = location.pathname;
+
   return (
     <nav className="Navbar">
-      <Link to="/"><h2>Home</h2></Link>
-      <Link to="/favorites"><h2>Favorites</h2></Link>
+      <Link
+        className={path === '/' ? 'Route-link Route-active' : 'Route-link'}
+        to="/">
+        <h2>Home</h2>
+      </Link>
+      <Link
+        className={path === '/favorites' ? 'Route-link Route-active' : 'Route-link'}
+        to="/favorites">
+        <h2>Favorites</h2>
+      </Link>
     </nav>
   );
 }
